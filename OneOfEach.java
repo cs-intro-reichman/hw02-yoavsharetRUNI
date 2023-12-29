97bc15ed77a3a7cf8a13a4950cc552562713de4b
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-29 19:30:49 +0200

    finished OneOfEach; before autotest

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,23 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		boolean boy_flag = false, girl_flag = false;
+		int num_of_childern = 0;
+
+		// rand gender logic: (0, 2] - 0: boy; 1: girl
+		do {
+			// rand kid gender
+			if((int)(Math.random()*2) == 0){
+				// a boy was born
+				System.out.print("b ");
+				boy_flag = true;
+			} else {
+				System.out.print("g ");
+				girl_flag = true;
+			}
+			num_of_childern++;
+		} while(!boy_flag || !girl_flag);
+		System.out.println();
+		System.out.println("You made it... and you now have " +  num_of_childern  + " children.");
 	}
 }
