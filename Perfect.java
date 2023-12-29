59751e1d9f5059b9d12c02b559222222e30e0248
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-29 15:40:08 +0200

    finished perfect; before autotest

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,23 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int usr_input = Integer.parseInt(args[0]);
+		int sum_devisors = 0;
+		String output = usr_input + " is a perfect number since " + usr_input + " = ";
+		
+		for(int i=1; i<usr_input; i++){
+			if(usr_input % i == 0){
+				sum_devisors += i;
+				output += (i + " + ");
+			}
+		}
+		
+		output = output.substring(0, output.length()-3);  //removing the last " + "
+		if(sum_devisors != usr_input){
+			//User input is a perfect number
+			output = usr_input + " is not a perfect number";
+		} 
+
+		System.out.println(output);
 	}
 }
