f9abca673847cf031c4218745c1fba3d4eeb49c8
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-29 15:23:32 +0200

    finished inOrder; before Autotest

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,14 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int MAX_RAND_VAL = 10; // range is [0, 10)
+		int new_rand = (int)(Math.random() * MAX_RAND_VAL);
+		int last_rand_val = -1;
+		do {
+			last_rand_val = new_rand;
+			System.out.print(last_rand_val + " ");
+			// generate new random number
+			new_rand = (int)(Math.random() * MAX_RAND_VAL);
+		} while (new_rand >= last_rand_val);
 	}
 }
