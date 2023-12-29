58e94a08c1d6c72a4b3a6afc80c0a84d4e4e9535
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-29 14:57:58 +0200

    finished Reverse; before Autotest

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,17 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String usr_input = args[0];
+		int input_length = usr_input.length();
+
+		// prints reversed usr input
+		for(int i = input_length-1; i>=0; i--){
+			System.out.print(usr_input.charAt(i));
+		}
+		System.out.println();
+
+		//finding the middle char, and outputs it
+		int middle_index = input_length % 2 == 0 ? (input_length/2)-1 : ((input_length+1)/2)-1;
+		System.out.println("The middle character is " + usr_input.charAt(middle_index));
 	}
 }
