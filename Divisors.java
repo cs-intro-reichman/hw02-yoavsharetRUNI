dbc07c5f8586e6638ab1d3834a0533903e5c1714
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-29 14:44:30 +0200

    finished Divisors; before autotest

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,12 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int number_from_usr = Integer.parseInt(args[0]);
+		
+		for (int i = 1; i<=number_from_usr; i++){
+			if(number_from_usr % i == 0){
+				System.out.println(i); 
+			}
+		}
 	}
 }
