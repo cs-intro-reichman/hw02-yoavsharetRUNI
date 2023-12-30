31f6fc75994e220468af9e602ab79021acfe1cef
yoavsharetRUNI <yoav.sharet@post.runi.ac.il>
2023-12-30 20:36:46 +0200

    Improved code structure

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,14 +6,9 @@ public class DamkaBoard {
 		int input_board_dimensions = Integer.parseInt(args[0]);
 
 		for(int i = 0; i<input_board_dimensions; i++){
-			if(i % 2 == 0){
-				for(int j = 0; j<input_board_dimensions; j++){
-					System.out.print("* ");
-				}
-			} else {
-				for(int j = 0; j<input_board_dimensions; j++){
-					System.out.print(" *");
-				}
+			String output_token = (i % 2 == 0) ? "* ": " *";
+			for(int j = 0; j<input_board_dimensions; j++){
+				System.out.print(output_token);
 			}
 			System.out.println();
 		}
